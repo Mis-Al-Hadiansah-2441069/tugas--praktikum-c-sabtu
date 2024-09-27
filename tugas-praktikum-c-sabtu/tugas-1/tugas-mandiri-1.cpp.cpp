6d4593f29ef3b56ad35b20133d541dd548b96a31
unknown <mis.hadiansah@gmail.com>
2024-09-27 15:06:13 +0700

    first commit

---
+++ tugas-praktikum-c-sabtu/tugas-1/tugas-mandiri-1.cpp.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	printf("NIM 2441069.\n");
+	printf("NAMA MIS'AL HADIANSAH.\n");
+	printf("TEKNIK INFORMATIKA.");
+	return 0;
+}
---
+++ tugas-praktikum-c-sabtu/tugas-1/tugas-mandiri-2.cpp.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+	int PN, LB, LS;
+	
+	PN=10;
+	
+	LB=7;
+	
+	
+	printf("Diketahui panjang persegi 10CM.\n");
+	
+	printf("Dan lebar persegi 7CM.\n");
+	
+	printf("Maka luas persegi adalah...?\n");
+	
+	LS=PN*LB;
+	
+	printf("%d",LS);
+	
+	return 0;
+}
