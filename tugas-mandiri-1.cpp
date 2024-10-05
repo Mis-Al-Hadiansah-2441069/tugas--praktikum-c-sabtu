3c3997434ae12daf92fef068258af0122d28017d
Mis-Al-Hadiansah-2441069 <mis.hadiansah@gmail.com>
2024-10-05 11:02:52 +0700

    Add files via upload

---
+++ tugas-mandiri-1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main ()
+{
+	printf("nama : MIS'AL HADIANSAH'");
+	printf("Nim : 2441069");
+	int alas = 8;
+	int tinggi = 5;
+	int hasil = alas * tinggi / 2;
+	printf("luas segitiga dengan ukuran alas 8 cm dan tinggi 5 cm adalah %d cm",hasil);
+	return 0;
+}
